378eaa88c406f3a7bcf961702a87ed7270c20dc0
Idaho Frost <frostidaho@gmail.com>
2016-09-30 22:10:12 -0400

    Update ex_rofi to use prettier printing

--- examples/ex_rofi.py
+++ examples/ex_rofi.py
@@ -1,19 +1,36 @@
 #!/usr/bin/env python
 import string
+import textwrap
+import pprint
+
 from dynmen import Menu
+
+
 rofi = Menu(command=('rofi', '-fullscreen', '-dmenu', '-i'))
 
-d_string = vars(string)
-d_string = {k:v for k,v in d_string.items() if not k.startswith('_')}
+exampl_inp_dict = vars(string)
+exampl_inp_dict = {k:v for k,v in exampl_inp_dict.items() if not k.startswith('_')}
 
-print('Launching rofi - given a dict')
-output = rofi(d_string)
-print(output, '\n')
+def print_obj(obj, prefix='    '):
+    txt = pprint.pformat(obj)
+    lines = []
+    for line in txt.splitlines():
+        line = textwrap.indent(line, prefix)
+        lines.append(line)
+    print('\n'.join(lines))
 
-print('Launching rofi - first sorting entries of dict')
-output2 = rofi.sort(d_string)
-print(output2, '\n')
+def run_n_print(entries, fn_str):
+    fn = globals()[fn_str.split('.')[0]]
+    for attr in fn_str.split('.')[1:]:
+        fn = getattr(fn, attr)
+    print("\nLAUNCHING '{}' with -".format(fn_str))
+    print_obj(entries)
+    output = fn(entries)
+    print('OUTPUT IS -')
+    print_obj(output)
+    return output
 
-print('Launching rofi - given a list')
-output3 = rofi(list(d_string))
-print(output3, '\n')
+run_n_print(exampl_inp_dict, 'rofi')
+run_n_print(exampl_inp_dict, 'rofi.sort')
+run_n_print(list(exampl_inp_dict), 'rofi')
+
