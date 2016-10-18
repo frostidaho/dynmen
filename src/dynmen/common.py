fe2d19a1dcaa7a50d34333d99076d35ae1312fc8
Idaho Frost <frostidaho@gmail.com>
2016-10-18 16:56:59 -0400

    Remove code duplication

--- src/dynmen/common.py
+++ src/dynmen/common.py
@@ -80,18 +80,3 @@ class TraitMenu(Menu):
                                      isinstance(cls.__dict__.get(x), (Option, Flag))]
         return self._list_opts_names
 
-
-class Rofi(TraitMenu):
-    fullscreen = Flag('fullscreen', flag='-fullscreen')
-    case_insensitive = Flag('case_insensitive', flag='-i')
-    # prompt = Option('prompt', default='', opt='-p')
-    prompt = Option('prompt', default='Input: ', opt='-p')
-    lines = Option('lines', default=15, opt='-l')
-    hide_scrollbar = Flag('hide_scrollbar', flag='-hide-scrollbar')
-
-    def __init__(self, *rofi_args, **kwargs):
-        super(Rofi, self).__init__(['rofi', '-dmenu'])
-        self.command.extend(rofi_args)
-        for k,v in kwargs.items():
-            setattr(self, k, v)
-
