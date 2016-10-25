19a3fe36c7b26f1ad524fe12f75d61d07477cbbc
Idaho Frost <frostidaho@gmail.com>
2016-10-25 18:07:19 -0400

    Add a number of options to rofi

--- src/dynmen/rofi.py
+++ src/dynmen/rofi.py
@@ -2,19 +2,66 @@
 from dynmen.common import TraitMenu, Flag, Option
 
 class Rofi(TraitMenu):
-    fullscreen = Flag('fullscreen', flag='-fullscreen', info='Display menu using entire screen')
-    case_insensitive = Flag('case_insensitive', flag='-i', info='Case insensitive matching')
+    ########################################
+    # Common
+    ######################################## 
     prompt = Option(
         'prompt',
         default='Input: ',
         flag='-p',
         info='Display text to the left of the input',
     )
+    case_insensitive = Flag('case_insensitive', flag='-i',
+                            info='Case insensitive matching')
     lines = Option(
         'lines',
-        default=15,
+        default=None,
         flag='-l',
         info='Number of lines to display. (Does not work with fullscreen)',
+        type=int,
+    )
+    fullscreen = Flag('fullscreen', flag='-fullscreen',
+                      info='Display menu using entire screen')
+    password = Flag(
+        'password',
+        flag='-password',
+        info='Hide the input text. This should not be considered secure!',
+    )
+
+    ########################################
+    # Multiline entry options
+    ######################################## 
+    element_height = Option(
+        'element_height',
+        type=int,
+        flag='-eh',
+        info='The height of a field in lines. Use in conjunction with -sep.',
+    )
+    separator = Option(
+        'separator',
+        type=str,
+        flag='-sep',
+        info='Separator for dmenu.'
+    )
+
+    ########################################
+    # Aesthetic options
+    ######################################## 
+    font = Option(
+        'font',
+        info='Font to use with rofi',
+        flag='-font',
+        type=str,
+    )
+    opacity = Option(
+        'opacity',
+        flag='-opacity',
+        info='Set window opacity (0-100).'
+    )
+    border_width = Option(
+        'border_width',
+        flag='-bw',
+        info='Set border width in pixels.',
         type=int,
     )
     hide_scrollbar = Flag(
