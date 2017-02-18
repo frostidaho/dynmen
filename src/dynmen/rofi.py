d19eeba7bb8204c48695765bbc7d19733d07368a
Idaho Frost <frostidaho@gmail.com>
2017-02-18 15:07:23 -0500

    Set '-dmenu' flag as default for Rofi menu

--- src/dynmen/rofi.py
+++ src/dynmen/rofi.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from os import path as _path
-from dynmen.common import TraitMenu as _TraitMenu
+from dynmen.common import (
+    TraitMenu as _TraitMenu,
+    Flag as _Flag,
+)
 from dynmen.generator import (AddOptions as _AddOptions,
                               load_options as _load_options,)
 
@@ -11,4 +14,13 @@ _path = _path.join(_dirname, 'data/rofi_opts.json')
 @_AddOptions(*_load_options(_path))
 class Rofi(_TraitMenu):
     _base_command = ['rofi']
+
+    # Explicitly add dmenu flag and set it to True by default.
+    # This makes rofi read choices from stdin
+    dmenu = _Flag(
+        'dmenu',
+        default=True,
+        info="Start in dmenu mode.",
+        flag='-dmenu',
+    )
 
