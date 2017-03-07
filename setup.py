f818d0f5720268c26a88b32a17759fae320cc8f3
Idaho Frost <frostidaho@gmail.com>
2017-03-07 12:56:16 -0500

    Update setup.py and bump version

--- setup.py
+++ setup.py
@@ -7,10 +7,20 @@ from glob import glob
 
 setup(
     name='dynmen',
-    version='0.0.5',
+    version='0.0.6',
     description='dynmen is an interface to dynamic menus, like dmenu, rofi, or fzf.',
     author='Idaho Frost',
     author_email='frostidaho@gmail.com',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: Unix",
+    ],
     url='https://github.com/frostidaho/dynmen',
     # py_modules = ['dynmen'],
     packages=find_packages('src'),
