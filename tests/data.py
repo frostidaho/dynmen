a043aca4b4137cd97a122244afa47a24323e4598
Idaho Frost <frostidaho@gmail.com>
2017-03-09 17:40:32 -0500

    Add test including support for a transformation fn on launch

--- tests/data.py
+++ tests/data.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import json as _json
 from collections import OrderedDict as _OrderedDict
+import os as _os
+
+_thisdir = _os.path.dirname(__file__)
 
 class MenuData(object):
+    _data_files = [
+        'data.json',
+    ]
     def __init__(self, *args, **kwargs):
-        with open('data.json', 'rb') as fdata:
-            dat = _json.load(fdata)
-
-        for k,v in dat.items():
-            setattr(self, k, v)
-
+        dfiles = [_os.path.join(_thisdir, x) for x in self._data_files]
+        for fpath in dfiles:
+            with open(fpath, 'rb') as fdata:
+                dat = _json.load(fdata)
+                for k,v in dat.items():
+                    setattr(self, k, v)
         self.people = _OrderedDict(self.people)
 
 
--- tests/test_cmd_blocking.py
+++ tests/test_cmd_blocking.py
@@ -1,29 +1,43 @@
 from dynmen.cmd import blocking
+from data import MenuData
 
 launch = blocking.launch
+
+def cat_identity(input_fn, stdout):
+    out = launch(['cat'], input_fn)
+    assert out.stdout == stdout
+    assert out.stderr == b''
+    assert out.returncode == 0
 
 def test_launch_bytes():
     inp = b'sfd8zjzunfzl3'
     inpfn = lambda: inp
-    out = launch(['cat'], inpfn)
-    assert out.stdout == inp
-    assert out.stderr == b''
-    assert out.returncode == 0
+    cat_identity(inpfn, inp)
 
 def test_launch_list_of_bytes():
     inp = [b'1', b'2', b'3']
     inpfn = lambda: b'@'.join(inp)
-    out = launch(['cat'], inpfn)
-    assert out.stdout == b'@'.join(inp)
-    assert out.stderr == b''
-    assert out.returncode == 0
+    cat_identity(inpfn, b'@'.join(inp))
 
 def test_launch_list_of_bytes2():
     inp = [b'4', b'5', b'6']
     entry_sep = b'\n'
     inpfn = lambda: entry_sep.join(inp)
-    out = launch(['cat'], inpfn)
-    assert out.stdout == entry_sep.join(inp)
-    assert out.stderr == b''
-    assert out.returncode == 0
+    cat_identity(inpfn, entry_sep.join(inp))
 
+def test_transform_res():
+    md = MenuData()
+    lpeople = list(md.people)
+    people_txt = '\n'.join(lpeople)
+    inpfn = lambda: people_txt.encode()
+    cat_identity(inpfn, people_txt.encode())
+
+    person = lpeople[10]
+    out = launch(['grep', person], inpfn)
+    assert out.stdout.startswith(person.encode())
+    out2 = out.stdout.decode().strip()
+    assert out2 == person
+
+    person = lpeople[23]
+    out = launch(['grep', person], inpfn, lambda x: x.stdout.decode())
+    assert out == person + '\n'
