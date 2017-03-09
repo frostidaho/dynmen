cec95b18b628d4f5199386974c0228a458cc59e5
Idaho Frost <frostidaho@gmail.com>
2017-03-08 21:29:09 -0500

    Add some tests for cmd.blocking

--- src/dynmen/cmd/blocking.py
+++ src/dynmen/cmd/blocking.py
@@ -1,14 +1,14 @@
 import subprocess as _sp
 from . import ProcStatus, _to_bytes
 
-def launch(cmd, stdin):
+def launch(cmd, stdin, entry_sep=b''):
     PIPE = _sp.PIPE
     proc = _sp.Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    bstr = _to_bytes(stdin)
+    bstr = _to_bytes(stdin, entry_sep)
     stdout, stderr = proc.communicate(bstr)
     try:
-        p.terminate()
+        proc.terminate()
     except ProcessLookupError:
         pass
-    return ProcStatus(stdout, stderr, p.returncode)
+    return ProcStatus(stdout, stderr, proc.returncode)
 
---
+++ tests/test_cmd_blocking.py
@@ -0,0 +1,26 @@
+from dynmen.cmd import blocking
+
+launch = blocking.launch
+
+def test_launch_bytes():
+    inp = b'sfd8zjzunfzl3'
+    out = launch(['cat'], inp)
+    assert out.stdout == inp
+    assert out.stderr == b''
+    assert out.returncode == 0
+
+def test_launch_list_of_bytes():
+    inp = [b'1', b'2', b'3']
+    out = launch(['cat'], inp)
+    assert out.stdout == b''.join(inp)
+    assert out.stderr == b''
+    assert out.returncode == 0
+
+def test_launch_list_of_bytes2():
+    inp = [b'4', b'5', b'6']
+    entry_sep = b'\n'
+    out = launch(['cat'], inp, entry_sep)
+    assert out.stdout == entry_sep.join(inp)
+    assert out.stderr == b''
+    assert out.returncode == 0
+
