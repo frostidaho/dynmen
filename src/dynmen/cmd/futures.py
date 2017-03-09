f5d505f37fb3794a170a0e5221bf3fd53919e3aa
Idaho Frost <frostidaho@gmail.com>
2017-03-09 13:46:24 -0500

    Fix version compatibility bug in cmd.futures

--- src/dynmen/cmd/futures.py
+++ src/dynmen/cmd/futures.py
@@ -2,8 +2,9 @@ from .blocking import launch as _launch
 from concurrent.futures import ThreadPoolExecutor
 
 def launch(cmd, stdin, entry_sep=b''):
-    tname = 'dynmen_cmd_futures'
-    executor = ThreadPoolExecutor(max_workers=1, thread_name_prefix=tname)
+    # Do not pass a thread_name_prefix to ThreadPoolExecutor
+    # as that is only for py36 and beyond.
+    executor = ThreadPoolExecutor(max_workers=1)
     future = executor.submit(_launch, cmd, stdin, entry_sep)
     future.add_done_callback(lambda x: executor.shutdown(wait=False))
     return future
