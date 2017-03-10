fb69a107f80945e38452d53e593151c1168d06f6
Idaho Frost <frostidaho@gmail.com>
2017-03-10 15:37:12 -0500

    Decent update to integration test. It now runs in xephyr

--- tests/integration.py
+++ tests/integration.py
@@ -5,42 +5,80 @@ import Xlib.XK
 from time import sleep
 import os
 
+class xcontrol(object):
+    def __init__(self, n_display=0):
+        self.display_str = ':{:d}'.format(n_display)
+        self.display = Display(self.display_str)
+        display = self.display
 
-display = Display(os.getenv('DISPLAY'))
+        getkey = lambda x: display.keysym_to_keycode(Xlib.XK.string_to_keysym(x))
+        self.getkey = getkey
 
-getkey = lambda x: display.keysym_to_keycode(Xlib.XK.string_to_keysym(x))
+        # retkey = getkey('Return')
+        spacekey = getkey('space')
 
-retkey = getkey('Return')
-spacekey = getkey('space')
+        self.d_keys = {
+            ' ': spacekey,
+        }
 
-d_keys = {
-    ' ': spacekey,
-}
 
-def str_to_keycodes(txt):
-    for char in txt:
-        try:
-            yield d_keys[char]
-        except KeyError:
-            val = getkey(char)
-            d_keys[char] = val
-            yield val
+    def str_to_keycodes(self, txt):
+        d_keys = self.d_keys
+        getkey = self.getkey
+        for char in txt:
+            try:
+                yield d_keys[char]
+            except KeyError:
+                val = getkey(char)
+                d_keys[char] = val
+                yield val
 
-def type_str(txt):
-    display.sync()
-    sleep(0.3)
-    for key in str_to_keycodes(txt):
+    def type_str(self, txt):
+        display = self.display
+        display.sync()
+        sleep(0.3)
+        for key in self.str_to_keycodes(txt):
+            sleep(0.1)
+            fake_input(display, X.KeyPress, key)
+            fake_input(display, X.KeyRelease, key)
+            display.sync()
+
+    def hit_enter(self):
+        retkey = self.getkey('Return')
         sleep(0.1)
-        fake_input(display, X.KeyPress, key)
-        fake_input(display, X.KeyRelease, key)
+        display = self.display
+        display.sync()
+        fake_input(display, X.KeyPress, retkey)
+        fake_input(display, X.KeyRelease, retkey)
         display.sync()
 
-def hit_enter():
-    retkey = getkey('Return')
-    sleep(0.1)
-    display.sync()
-    fake_input(display, X.KeyPress, retkey)
-    fake_input(display, X.KeyRelease, retkey)
-    display.sync()
+
+def start_xephyr():
+    def build_cmd(n_display=0):
+        cmd = ['Xephyr']
+        display = ':{:d}'.format(n_display)
+        cmd.append(display)
+        cmd.extend(['-screen', '800x600'])
+        return cmd
+
+    import subprocess as sp
+    for idx in range(20):
+        cmd = build_cmd(idx)
+        p = sp.Popen(cmd)
+        sleep(0.1)
+        retcode = p.poll()
+        if retcode is None:
+            break
+        else:
+            try:
+                p.terminate()
+            except:
+                pass
+            continue
+    else:
+        raise ValueError("Couldn't start server")
+    return idx
+
+# x = start_xephyr()
 
 
--- tests/test_menu_rofi.py
+++ tests/test_menu_rofi.py
@@ -1,16 +1,23 @@
 import pytest
 
 integration = pytest.importorskip('integration')
+
+import os
+n_display = integration.start_xephyr()
+xctrl = integration.xcontrol(n_display)
+os.environ['DISPLAY'] = ':{:d}'.format(n_display)
+
 from dynmen.menu import Menu
 from time import sleep
+
 
 def test_simple():
     menu = Menu(['rofi', '-dmenu'])
     menu.process_mode = 'futures'
     res = menu(['a', 'b', 'c'])
     sleep(1.0)
-    integration.type_str('b')
-    integration.hit_enter()
+    xctrl.type_str('b')
+    xctrl.hit_enter()
     out = res.result()
     assert out.selected == 'b'
     assert out.value == None
