6d487b873f7b5f4ac026e923603ef96707bcc0f2
Idaho Frost <frostidaho@gmail.com>
2017-03-12 16:18:42 -0400

    Add a docstring to the main __init__.py

--- src/dynmen/__init__.py
+++ src/dynmen/__init__.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+"""
+dynmen - A simple python interface to dynamic menus like dmenu or rofi
+
+import dynmen
+menu = dynmen.Menu(['dmenu', '-fn', 'Sans-30'])
+output = menu({'a': 1, 'b': 2, 'c': 3})
+
+You can make the menu non-blocking by setting:
+   menu.process_mode = 'futures'
+
+Please see the repository for more examples:
+    https://github.com/frostidaho/dynmen
+"""
 from .menu import Menu, MenuError
 del menu
 
