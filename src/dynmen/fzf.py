8518f3fba670b71fde9eef85a4c85525d68d9776
Idaho Frost <frostidaho@gmail.com>
2017-03-11 21:40:38 -0500

    Comment-out fzf for now

--- src/dynmen/fzf.py
+++ src/dynmen/fzf.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
-from dynmen.common import TraitMenu, Flag, Option
+# # -*- coding: utf-8 -*-
+# from dynmen.common import TraitMenu, Flag, Option
 
 
-class FZF(TraitMenu):
-    _base_command = ['fzf']
-    case_insensitive = Flag(
-        'case_insensitive',
-        flag='-i',
-        info='Case insensitive matching',
-    )
-    prompt = Option(
-        'prompt',
-        default='Input: ',
-        flag='--prompt',
-        info='Display text to the left of the input',
-    )
+# class FZF(TraitMenu):
+#     _base_command = ['fzf']
+#     case_insensitive = Flag(
+#         'case_insensitive',
+#         flag='-i',
+#         info='Case insensitive matching',
+#     )
+#     prompt = Option(
+#         'prompt',
+#         default='Input: ',
+#         flag='--prompt',
+#         info='Display text to the left of the input',
+#     )
