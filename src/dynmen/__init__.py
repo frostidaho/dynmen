8c3fecb4fc3a2787d6c7c6a5ce015c9e01941e3d
Idaho Frost <frostidaho@gmail.com>
2017-03-11 22:39:28 -0500

    Add factory functions for dmenu and rofi in root dynmen file

--- src/dynmen/__init__.py
+++ src/dynmen/__init__.py
@@ -2,3 +2,11 @@
 from .menu import Menu, MenuError
 del menu
 
+def new_dmenu(**kwargs):
+    from .dmenu import DMenu
+    return DMenu(**kwargs)
+
+def new_rofi(**kwargs):
+    from .rofi import Rofi
+    return Rofi(**kwargs)
+
