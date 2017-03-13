ad54e6d71b4bff792304e12e013e8df522ba25a4
Idaho Frost <frostidaho@gmail.com>
2017-03-12 21:57:26 -0400

    Fix test_dmenu.py

--- tests/test_dmenu.py
+++ tests/test_dmenu.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from dynmen.dmenu import DMenu
 from dynmen import new_dmenu
+import pytest
 
 def test_import():
     menu = DMenu()
+    assert menu._trait_transformed['i'] == []
+    assert menu.i is False
     menu.i = True
-    assert menu.i.value is True
+    assert menu.i is True
+    assert menu._trait_transformed['i'] == ['-i']
 
 def test_import2():
     menu = new_dmenu(i=True)
-    assert menu.i.value is True
-
+    assert menu.i is True
+    assert menu._trait_transformed['i'] == ['-i']
