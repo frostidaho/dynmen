fa2ff2e230ea4497f5996ead5f985b08b22fb9f6
Idaho Frost <frostidaho@gmail.com>
2017-03-17 16:28:38 -0400

    Add DMenu class testing to test_menu_dmenu.py

--- tests/test_menu_dmenu.py
+++ tests/test_menu_dmenu.py
@@ -10,15 +10,26 @@ pytestmark = pytest.mark.skipif(
 
 xctrl = fixtures.xctrl
 from dynmen.menu import Menu
+from dynmen.dmenu import DMenu
 from time import sleep
+
+def pytest_generate_tests(metafunc):
+    if 'dmenu' in metafunc.fixturenames:
+        metafunc.parametrize(
+            'dmenu',
+            ['Menu', 'DMenu'],
+            indirect=True,
+        )
 
 MAX_WAIT = 3.0
 SPAWN_WAIT = 1.0
 
 @pytest.fixture(scope='function')
-def dmenu(xctrl):
-    menu = Menu(['dmenu'], process_mode='futures')
-    return menu
+def dmenu(request, xctrl):
+    if request.param == 'Menu':
+        return Menu(['dmenu'], process_mode='futures')
+    elif request.param == 'DMenu':
+        return DMenu(process_mode='futures')
 
 def assert_future(future, selected, value):
     out = future.result(MAX_WAIT)
@@ -47,7 +58,10 @@ def test_non_matching_dict(dmenu, xctrl):
     assert_future(res, 'some other string', None)
 
 def test_case_insensitive_dict(dmenu, xctrl):
-    dmenu.command.append('-i')
+    try:
+        dmenu.command.append('-i')
+    except AttributeError:
+        dmenu.case_insensitive = True
     future = dmenu(['aaa', 'bbb', 'ccc'])
     sleep(SPAWN_WAIT)
     xctrl.type_str('CC')
