090ca0e14621d287e9f5d1d301589d99d95cd224
Idaho Frost <frostidaho@gmail.com>
2017-03-17 17:09:49 -0400

    Remove unused function

--- src/dynmen/cmd/__init__.py
+++ src/dynmen/cmd/__init__.py
@@ -3,8 +3,3 @@ from collections import namedtuple as _namedtuple
 
 ProcStatus = _namedtuple('ProcStatus', 'stdout stderr returncode')
 
-
-def _to_bytes(obj, entry_sep=b''):
-    if isinstance(obj, bytes):
-        return obj
-    return entry_sep.join(obj)
